b51fefa80cbea8d146b99bf82bb7cfb8d422cc14
Yanndroid <yann.ritzert@yahoo.fr>
2021-02-21 10:40:00 +0100

    Version 1.0.0

---
+++ app/src/main/java/de/dlyt/yanndroid/samsung/SplashActivity.java
@@ -0,0 +1,26 @@
+package de.dlyt.yanndroid.samsung;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent();
+                intent.setClass(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        }, 750);
+
+    }
+}
