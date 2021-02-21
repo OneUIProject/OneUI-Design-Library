dd38135131d76f8a99aef21fbc1b5311f095948b
Yanndroid <yann.ritzert@yahoo.fr>
2021-02-21 10:13:08 +0100

    Version 1.0.0

---
+++ app/src/main/java/de/dlyt/yanndroid/samsung/MainActivity.java
@@ -0,0 +1,135 @@
+package de.dlyt.yanndroid.samsung;
+
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.content.ContextCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.appbar.AppBarLayout;
+import com.google.android.material.card.MaterialCardView;
+import com.google.android.material.switchmaterial.SwitchMaterial;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        initToolbar();
+        initDrawer();
+
+
+
+        SwitchMaterial aswitch1111 = findViewById(R.id.aswitch1111);
+        SwitchMaterial barswitch = findViewById(R.id.aswitchbar);
+        aswitch1111.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                barswitch.setEnabled(!isChecked);
+            }
+        });
+
+
+
+
+
+    }
+
+
+
+    public void initDrawer() {
+        View content = findViewById(R.id.main_content);
+        DrawerLayout drawerLayout = findViewById(R.id.drawerLayout);
+        View drawer = findViewById(R.id.drawer);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+
+
+        ViewGroup.LayoutParams layoutParams = drawer.getLayoutParams();
+        layoutParams.width = (int) ((double) this.getResources().getDisplayMetrics().widthPixels / 1.19);
+        drawerLayout.setScrimColor(ContextCompat.getColor(getBaseContext(), R.color.drawer_dim_color));
+        drawerLayout.setDrawerElevation(0);
+
+        ActionBarDrawerToggle actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.opend, R.string.closed) {
+            @Override
+            public void onDrawerSlide(View drawerView, float slideOffset) {
+                super.onDrawerSlide(drawerView, slideOffset);
+                float slideX = drawerView.getWidth() * slideOffset;
+                content.setTranslationX(slideX);
+            }
+        };
+
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                drawerLayout.openDrawer(drawer, true);
+            }
+        });
+
+
+        View drawer_settings = findViewById(R.id.drawer_settings);
+
+        drawer_settings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getBaseContext(), "Settings", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+
+    public void initToolbar() {
+        /** Def */
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        AppBarLayout AppBar = findViewById(R.id.app_bar);
+
+        TextView expanded_title = findViewById(R.id.expanded_title);
+        TextView expanded_subtitle = findViewById(R.id.expanded_subtitle);
+        TextView collapsed_title = findViewById(R.id.collapsed_title);
+
+        /** 1/3 of the Screen */
+        ViewGroup.LayoutParams layoutParams = AppBar.getLayoutParams();
+        layoutParams.height = (int) ((double) this.getResources().getDisplayMetrics().heightPixels / 2.6);
+
+
+        /** Collapsing */
+        AppBar.addOnOffsetChangedListener(new AppBarLayout.OnOffsetChangedListener() {
+            @Override
+            public void onOffsetChanged(AppBarLayout appBarLayout, int verticalOffset) {
+                float percentage = (AppBar.getY() / AppBar.getTotalScrollRange());
+                expanded_title.setAlpha(1 - (percentage * 2 * -1));
+                expanded_subtitle.setAlpha(1 - (percentage * 2 * -1));
+                collapsed_title.setAlpha(percentage * -1);
+            }
+        });
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        return super.onOptionsItemSelected(item);
+    }
+
+}
