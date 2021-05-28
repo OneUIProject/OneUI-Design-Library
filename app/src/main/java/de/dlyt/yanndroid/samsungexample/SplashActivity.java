e681abe10843080c0a131d58df13f2a52a9a806d
Yanndroid <yann.ritzert@yahoo.fr>
2021-05-28 20:14:39 +0200

    sample

--- app/src/main/java/de/dlyt/yanndroid/samsungexample/SplashActivity.java
+++ app/src/main/java/de/dlyt/yanndroid/samsungexample/SplashActivity.java
@@ -17,7 +17,7 @@ public class SplashActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
-        SplashViewAnimated splashViewAnimated = findViewById(R.id.sss);
+        SplashViewAnimated splashViewAnimated = findViewById(R.id.splash);
 
         Handler handler = new Handler(Looper.getMainLooper());
         handler.postDelayed(() -> {
