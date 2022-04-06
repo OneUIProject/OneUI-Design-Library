15117b4b0499f75cafc400812863051027d31a38
Yanndroid <yanndroid.dev@gmail.com>
2022-04-06 19:21:56 +0200

    v2.4.0 - time, date, number pickers - theme dynamic drawables (oui3/4) - icons++ - improvements & bug/crash

--- app/src/main/java/de/dlyt/yanndroid/oneuiexample/fragments/MainActivitySecondFragment.java
+++ app/src/main/java/de/dlyt/yanndroid/oneuiexample/fragments/MainActivitySecondFragment.java
@@ -4,7 +4,6 @@ import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Toast;
 
 import androidx.appcompat.util.SeslMisc;
 
